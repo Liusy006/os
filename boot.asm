5399b696a1fecd48e0c0efcdc33aab38d9c1fb17
liusy <liusy_cn@163.com>
2019-08-03 06:21:59 -0700

    fix : show hello

--- boot.asm
+++ boot.asm
@@ -2,9 +2,25 @@
 ; A simple boot sector programer
 ;
 
-loop:
+mov ah, 0x0e
+
+mov al, 'H'
+int 0x10
+
+mov al, 'e'
+int 0x10
+
+mov al, 'l'
+int 0x10
+
+mov al, 'l'
+int 0x10
+
+mov al, 'o'
+int 0x10 
+;loop:
 	
-	jmp loop
+	;jmp loop
 
 times 510-($-$$) db 0
 
